aa499c26f6abbda4f410554dcba73b3789a98226
rutszo <93184355+rutszo@users.noreply.github.com>
2023-04-18 14:44:32 +0100

    Add files via upload
    base file for index

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+include("./connect.php");
+
+session_start();
+
+error_reporting(-1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    if (isset($_SESSION['loggedin'])){
+        echo "Estás logado";
+        ?>
+        <a href="logout.php">Sai aqui!</a>
+        <?php
+    } else {
+        echo "Não estás logado";
+        ?>
+        <a href="login.php">Entra aqui!</a>
+        <?php
+    }
+
+    ?>
+
+    
+    
+</body>
+</html>
