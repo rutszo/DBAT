8e0ddadeb269d8cd11c9a01621ab7a90b78cf939
rutszo <93184355+rutszo@users.noreply.github.com>
2023-04-18 14:42:09 +0100

    Update environment.php

--- environment.php
+++ environment.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost"; // Put your host server in here
-$username = "root"; // Put your username in here
-$password = ""; // Put your password in here
-$database = "dbat"; // Put your database name in here
+$host = "localhost"; // Put your host server here
+$username = "root"; // Put your username here
+$password = ""; // Put your password here
+$database = "dbat"; // Put your database name here
 
 ?>
